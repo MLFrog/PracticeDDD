a2683eb31e92ff49e56a8bb4a284809729ec568b
김동욱 <dongwook9763@naver.com>
2024-05-04 00:10:09 +0900

    init : init repository

--- src/main/java/com/mlfrog/practiceddd/PracticeDddApplication.java
+++ src/main/java/com/mlfrog/practiceddd/PracticeDddApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.mlfrog.practiceddd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mlfrog/practiceddd/PracticeDddApplicationTests.java
+++ src/test/java/com/mlfrog/practiceddd/PracticeDddApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.mlfrog.practiceddd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
